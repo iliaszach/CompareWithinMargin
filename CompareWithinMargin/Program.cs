3b544f238b7aefdc5de834d36ee661e9abaf70b7
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-06-22 10:56:36 +0300

    Add project files.

---
+++ CompareWithinMargin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareWithinMargin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CloseCompare(-4,-5));
+        }
+
+
+        public static int CloseCompare(double a, double b, double margin = 0)
+        {
+            //grab the difference between a,b
+            if (Math.Abs(a - b) <= margin) return 0;
+            return a<b? -1 : 1 ;
+        }
+
+    }
+}
